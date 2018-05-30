8f6463e93d8a9f3cde591ca2d4c776e66066a4f5
paulo marinho <paulo.marinho@personates.com>
2018-05-29 23:39:16 -0300

    002-banco

--- enquete/models.py
+++ enquete/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Pergunta(models.Model):
+    texto = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+
+class Opcao(models.Model):
+    questao = models.ForeignKey(Pergunta, on_delete=models.CASCADE)
+    opcao_pergunta = models.CharField(max_length=200)
+    votos = models.IntegerField(default=0)
