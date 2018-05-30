4a423967ffd1718c38dbc53a0c258a5006baf3f8
paulo marinho <paulo.marinho@personates.com>
2018-05-30 00:13:44 -0300

    006-adicionando rotas e paginas ao site

--- enquete/urls.py
+++ enquete/urls.py
@@ -4,4 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+
+    # ex: /polls/5/
+    path('<int:question_id>/', views.detalhes, name='detail'),
+    # ex: /polls/5/results/
+    path('<int:question_id>/resultados/', views.resultados, name='results'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/votar/', views.votar, name='vote'),
 ]
--- enquete/views.py
+++ enquete/views.py
@@ -5,4 +5,17 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Vamos as perguntas?")
+
+
+def detalhes(request, question_id):
+    return HttpResponse("pergunta %s." % question_id)
+
+
+def resultados(request, question_id):
+    response = "resultado da questao %s."
+    return HttpResponse(response % question_id)
+
+
+def votar(request, question_id):
+    return HttpResponse("cotando em  %s." % question_id)
